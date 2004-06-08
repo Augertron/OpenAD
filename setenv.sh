d88d0988337ec6c5334c441e491c8f5da3a7c4f1
eraxxon <eraxxon@ea457dc8-f85c-4c11-9f02-0de89cc6ba4c>
2004-06-08 15:34:48 +0000

    Improve error checking

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,7 @@
 # -*-Mode: sh;-*-
 # For sh variants
 
-# $Header: /m_home/m_utkej/Argonne/cvs2svn/cvs/OpenAD/setenv.sh,v 1.2 2004-06-08 14:35:31 eraxxon Exp $
+# $Header: /m_home/m_utkej/Argonne/cvs2svn/cvs/OpenAD/setenv.sh,v 1.3 2004-06-08 15:34:48 eraxxon Exp $
 
 #echo "$0 $*"
 
@@ -23,11 +23,21 @@ shell="--shell=sh"
 #############################################################################
 
 cmd="$prog $shell $*"
-
 #echo "--> $cmd"
-eval `$cmd`
+
+out=`$cmd`
 if [ $? -ne 0 ] ; then
-    echo "*** Error eval'ing: '$cmd' ***"
+    echo "*** Error eval'ing '$cmd' ***"
+    echo $out
     return 1
 fi
+
+eval $out
+if [ $? -ne 0 ] ; then
+    echo "*** Error eval'ing ***"
+    echo $out
+    return 1
+fi
+
 unset cmd
+unset out
