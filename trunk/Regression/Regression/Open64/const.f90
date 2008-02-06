fcbbd8120bba0e0da1fb96c7f778332e13c97bcf
utke <utke@ea457dc8-f85c-4c11-9f02-0de89cc6ba4c>
2008-02-06 19:49:43 +0000

    resync

--- trunk/Regression/Regression/Open64/const.f90
+++
@@ -1,8 +0,0 @@
-	program const
-	double precision:: talpha=2.D-4
-	double precision:: tbeta=1.234567D5
-	double precision, parameter :: PI=3.14159265358979323844D0
-	print*, talpha, tbeta
-	write(*,'(A)') '   3.14159265358979323844'
-	write(*,'(EN26.16E3)') PI
-	end program
