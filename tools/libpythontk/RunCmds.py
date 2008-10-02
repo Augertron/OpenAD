84f7d390a57723b9dc1d4180b5d891db96954f80
ju1 <ju1@ea457dc8-f85c-4c11-9f02-0de89cc6ba4c>
2008-10-02 21:29:04 +0000

    resync

--- tools/libpythontk/RunCmds.py
+++ tools/libpythontk/RunCmds.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-class RunCmdsException(Exception):
+class RunnerException(Exception):
   def __init__(self,reason):
     Exception.__init__(self,reason)
 
@@ -22,58 +22,58 @@ class CmdDesc:
   def setDesc(self,desc):
     self.desc = desc
 
+class Runner:
+  def __init__(self,verbose, interactive, logfnm, keepGoing):
+    self.verbose=verbose
+    self.interactive=interactive
+    self.logfnm=logfnm
+    self.keepGoing=keepGoing
 
-
-class RunCmds:
-  def __init__(self):
-    None
-
-  def RunCmds(self, cmdDescVecRef, verbose, interactive, logfnm):
+  def doit(self, cmdDescVecRef):
     for desc in cmdDescVecRef:
-      if interactive >= 1:
-        sys.stdout.write("Execute? "+desc.getCmd()+"\n")
-        ans =""
-        while (ans != "Y") and (ans != "y") and (ans != "N") and (ans !="n"):
-          sys.stdout.write("...[Y/n]\n")
-          ans = sys.stdin.read(1)
+      if self.interactive:
+        sys.stdout.write("Execute command: \""+desc.getCmd()+"\" (y)/n ? ")
+        ans = sys.stdin.read(1)
         if ans == 'n' or ans == 'N':
-          break
-      self.RunCmd(desc.getCmd(), desc.getDesc(), verbose, logfnm)
-  
-  def RunCmd(self, cmd, desc, verbose, logfnm):
+          continue
+      try:  
+        self.__doSingle(desc.getCmd(), desc.getDesc())
+      except RunnerException, e:
+        if self.keepGoing:
+          sys.stderr.write('ERROR: '+str(e)+'... continuing\n')
+        else:
+          raise e
+
+  def __doSingle(self,cmd,desc):
     dir = os.getcwd()
     outfnm = os.path.join(dir,"RunCmd-out.tmp~")
     f = open(outfnm,"w")
     f.close()
     cmd += " > "+outfnm+" 2>&1"
-    if logfnm:
+    if self.logfnm:
       # Append separator and command to log file
       try:
-        logfh = open(logfnm,"a")
+        logfh = open(self.logfnm,"a")
         logfh.write("*****************************************************************************\n\n")
         logfh.write(cmd+"\n")
         logfh.close()
       except IOError, e:
         print e
         sys.stderr.write("Unable to open logfh\n")
-
-    if verbose >= 1:
+    if self.verbose >= 1:
       sys.stdout.write(desc+"\n")
-
-    if verbose >= 2:
+    if self.verbose >= 2:
       sys.stdout.write("Executing "+cmd+"\n")
-
     try:
       ret=os.system(cmd)
       if ret!=0 : 
-	raise RunCmdsException(cmd+" return value "+str(ret))
+	raise RunnerException(cmd+" return value "+str(ret))
     except Exception, e:
-      self.AppendOutfileToLogfile(logfnm, outfnm)
+      self.__appendOutfileToLogfile(outfnm)
       os.system("cat "+outfnm)
       raise e
-
-    self.AppendOutfileToLogfile(logfnm, outfnm)
+    self.__appendOutfileToLogfile(outfnm)
     os.remove(outfnm)
 
-  def AppendOutfileToLogfile(self,logfnm, outfnm):
-    os.system("cat "+outfnm+" >> "+logfnm)
+  def __appendOutfileToLogfile(self, outfnm):
+    os.system("cat "+outfnm+" >> "+self.logfnm)
