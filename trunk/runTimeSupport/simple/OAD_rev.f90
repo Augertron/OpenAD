4b61ee9f8160f898b98cd04d67a862a701e6a879
ju1 <ju1@ea457dc8-f85c-4c11-9f02-0de89cc6ba4c>
2009-03-31 04:24:25 +0000

    resync

--- trunk/runTimeSupport/simple/OAD_rev.f90
+++ trunk/runTimeSupport/simple/OAD_rev.f90
@@ -55,7 +55,9 @@ module OAD_rev
   implicit none
 
   private
-  public :: modeType, our_rev_mode, mode_init
+  public :: modeType, our_rev_mode, OAD_revInit, &
+OAD_revPlain, OAD_revTape, OAD_revAdjoint, & 
+OAD_revStorePlain, OAD_revRestoreTape
   
   type modeType
      logical :: arg_store=.FALSE.
@@ -69,13 +71,33 @@ module OAD_rev
   
   type(modeType), save :: our_rev_mode
   
-  interface mode_init
-     module procedure mode_init_i
+  interface OAD_revInit
+     module procedure init
+  end interface
+  
+  interface OAD_revPlain
+     module procedure plain
+  end interface
+  
+  interface OAD_revTape
+     module procedure tape
+  end interface
+  
+  interface OAD_revAdjoint
+     module procedure adjoint
+  end interface
+  
+  interface OAD_revStorePlain
+     module procedure storePlain
+  end interface
+  
+  interface OAD_revRestoreTape
+     module procedure restoreTape
   end interface
   
 contains
 
-  subroutine mode_init_i()
+  subroutine init()
     our_rev_mode%arg_store=.FALSE.
     our_rev_mode%arg_restore=.FALSE.
     our_rev_mode%res_store=.FALSE.
@@ -83,6 +105,56 @@ contains
     our_rev_mode%plain=.TRUE.
     our_rev_mode%tape=.FALSE.
     our_rev_mode%adjoint=.FALSE.
-  end subroutine mode_init_i
+  end subroutine 
+
+  subroutine plain()
+    our_rev_mode%arg_store=.FALSE.
+    our_rev_mode%arg_restore=.FALSE.
+    our_rev_mode%res_store=.FALSE.
+    our_rev_mode%res_restore=.FALSE.
+    our_rev_mode%plain=.TRUE.
+    our_rev_mode%tape=.FALSE.
+    our_rev_mode%adjoint=.FALSE.
+  end subroutine 
+
+  subroutine tape()
+    our_rev_mode%arg_store=.FALSE.
+    our_rev_mode%arg_restore=.FALSE.
+    our_rev_mode%res_store=.FALSE.
+    our_rev_mode%res_restore=.FALSE.
+    our_rev_mode%plain=.FALSE.
+    our_rev_mode%tape=.TRUE.
+    our_rev_mode%adjoint=.FALSE.
+  end subroutine 
+
+  subroutine adjoint()
+    our_rev_mode%arg_store=.FALSE.
+    our_rev_mode%arg_restore=.FALSE.
+    our_rev_mode%res_store=.FALSE.
+    our_rev_mode%res_restore=.FALSE.
+    our_rev_mode%plain=.FALSE.
+    our_rev_mode%tape=.FALSE.
+    our_rev_mode%adjoint=.TRUE.
+  end subroutine 
+
+  subroutine storePlain()
+    our_rev_mode%arg_store=.TRUE.
+    our_rev_mode%arg_restore=.FALSE.
+    our_rev_mode%res_store=.FALSE.
+    our_rev_mode%res_restore=.FALSE.
+    our_rev_mode%plain=.TRUE.
+    our_rev_mode%tape=.FALSE.
+    our_rev_mode%adjoint=.FALSE.
+  end subroutine 
+
+  subroutine restoreTape()
+    our_rev_mode%arg_store=.FALSE.
+    our_rev_mode%arg_restore=.TRUE.
+    our_rev_mode%res_store=.FALSE.
+    our_rev_mode%res_restore=.FALSE.
+    our_rev_mode%plain=.FALSE.
+    our_rev_mode%tape=.TRUE.
+    our_rev_mode%adjoint=.FALSE.
+  end subroutine 
 
 end module OAD_rev
